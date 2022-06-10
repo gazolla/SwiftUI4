4a1714dfa90d362e61f2d20b6fce15977d515e73
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-06-10 10:55:46 -0300

    Initial Commit

---
+++ SwiftUI4/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI4
+//
+//  Created by sebastiao Gazolla Costa Junior on 10/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI4/SwiftUI4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI4App.swift
+//  SwiftUI4
+//
+//  Created by sebastiao Gazolla Costa Junior on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
