19be73e3b299e1058ef4abf6bb659acc0c99daf5
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-06-22 11:36:36 -0300

    MainView Added

--- SwiftUI4/MainView.swift
+++ SwiftUI4/MainView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 
+struct Content: Identifiable, Hashable{
+    let id = UUID()
+    let name: String
+    
+    static let loadedItems:[Content] = [
+        .init(name: "Chart"),
+        .init(name: "Sheet"),
+        .init(name: "Share Link"),
+        .init(name: "Image Renderer"),
+        .init(name: "Grid"),
+        .init(name: "Tap location view"),
+        .init(name: "Gauge"),
+        .init(name: "MultiDatePicker")
+    ]
+}
 
 struct MainView: View {
+    @State private var newFunctionalities : [Content] = Content.loadedItems
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(newFunctionalities) { functionality in
+                NavigationLink(functionality.name, value: functionality)
+            }
+            .navigationDestination(for: Content.self) { functionality in
+                selectFunctionality(name:functionality.name)
+            }
+            .navigationTitle("New in SwiftyUI 4")
+        }
+    }
+    
+    func selectFunctionality(name:String) -> some View {
+        switch name {
+        case "Chart":
+            return AnyView(ChartsView())
+        case "Sheet":
+            return AnyView(SheetView())
+        case "Share Link":
+            return AnyView(ShareLinkView())
+        case "Image Renderer":
+            return AnyView(ImageRendererView())
+        case "Grid":
+            return AnyView(GridView())
+        case "Tap location view":
+            return AnyView(TapLocationView())
+        case "Gauge":
+            return AnyView(GaugeView())
+        case "MultiDatePicker":
+            return AnyView(MultiDatePickerView())
+        default:
+            return AnyView(UnderConstruction())
         }
     }
 }
---
+++ SwiftUI4/UnderConstruction.swift
@@ -0,0 +1,20 @@
+//
+//  UnderConstruction.swift
+//  SwiftUI4
+//
+//  Created by sebastiao Gazolla Costa Junior on 22/06/22.
+//
+
+import SwiftUI
+
+struct UnderConstruction: View {
+    var body: some View {
+        Text("Under Construction")
+    }
+}
+
+struct UnderConstruction_Previews: PreviewProvider {
+    static var previews: some View {
+        UnderConstruction()
+    }
+}
