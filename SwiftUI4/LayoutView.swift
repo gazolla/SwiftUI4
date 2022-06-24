24e1a4be541118269ed74ee04d9f43096b4f80a1
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-06-24 10:04:28 -0300

    TextView modifications

--- SwiftUI4/LayoutView.swift
+++ SwiftUI4/LayoutView.swift
@@ -8,32 +8,8 @@
 import SwiftUI
 
 struct LayoutView: View {
-    @State private var myText = ""
-    @State private var isBlack = false
     var body: some View {
-        VStack{
-            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                .lineLimit(3...6)
-                .padding()
-            
-            Text("Changing Fonts!")
-                .font(.largeTitle)
-                .fontWeight(isBlack ? .black : .ultraLight)
-            
-            Button("Tap here to change font") {
-                withAnimation {
-                    isBlack.toggle()
-                }
-            }
-           
-            
-            TextField("Enter your text here", text: $myText, axis:.vertical)
-                .textFieldStyle(.roundedBorder)
-                .lineLimit(...5)
-            
-            
-        }
-        
+        Text("Hello, World!")
     }
 }
 
--- SwiftUI4/TextView.swift
+++ SwiftUI4/TextView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct TextView: View {
+    
+    @State private var isBlack = false
+    @State private var myText = ""
+   
+    
     var body: some View {
-        Text("Hello, World!")
-    }
+        VStack{
+           
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                .lineLimit(3...6)
+                .padding()
+            
+            Spacer()
+            
+            Text("Changing Fonts!")
+                .font(.largeTitle)
+                .fontWeight(isBlack ? .black : .ultraLight)
+            
+            Button("Tap here to change font") {
+                withAnimation {
+                    isBlack.toggle()
+                }
+            }
+           
+            Spacer()
+            
+            TextField("Enter your text here", text: $myText, axis:.vertical)
+                .textFieldStyle(.roundedBorder)
+                .lineLimit(...5)
+           
+            Spacer()
+        }
+        
+    }    
 }
 
 struct TextView_Previews: PreviewProvider {
